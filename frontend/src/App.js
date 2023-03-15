84650ac70ba93bf397d2b0173a24ffb951d80f41
coloniablace <bcolonia0712@gmail.com>
2023-03-15 11:02:50 +0800

    Frontend_Routing_List of Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">O-Shop</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">O-Shop</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+  products: [
+    {
+      name: 'Wall Mounted Basket Storage Rack',
+      slug: 'basket-storage-rack',
+      category: 'Bathroom Organizers',
+      image: '/images/basket_storage_rack.jpg', // 679px × 829px
+      price: 369,
+      countInStock: 20,
+      brand: 'Yamazaki',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        '8 Pack Wire Storage Baskets Household Metal Wall-Mounted Containers Organizer Bins',
+    },
+    {
+      name: 'Clothes Stackable Rack',
+      slug: 'clothes-stackable-rack',
+      category: 'Bedroom Organizers',
+      image: '/images/clothes_stackable_rack.jpg',
+      price: 299,
+      countInStock: 15,
+      brand: 'Zara',
+      rating: 4.8,
+      numReviews: 12,
+      description:
+        'Pinkpum Stackable Plastic Storage Basket-Foldable Closet Organizers and Storage Bins 4 Pack-Drawer Shelf Storage Container for Wardrobe Cupboard Kitchen Bathroom Office 4L',
+    },
+    {
+      name: 'Desk Storage Rack',
+      slug: 'desk-storage-rack',
+      category: 'Office Organizers',
+      image: '/images/desk_storage_rack.jpg',
+      price: 137,
+      countInStock: 15,
+      brand: 'CB2',
+      rating: 4.9,
+      numReviews: 14,
+      description:
+        'Desktop Bookshelf Desk Storage Organizer Adjustable Wood Desktop Display Shelf Rack Counter Office Storage Rack Top Bookcase - Free Style Display Natural Stand Office Supplies Desk Organizer (Brown)',
+    },
+    {
+      name: 'Kitchen Pan Organizer',
+      slug: 'kitchen-pan-organizer',
+      category: 'Kitchen Organizers',
+      image: '/images/kitchen_pan_organizer.jpg',
+      price: 329,
+      countInStock: 25,
+      brand: 'MoMA',
+      rating: 4.8,
+      numReviews: 16,
+      description: 'Pan Rack Organizer for Kitchen Cabinet',
+    },
+  ],
+};
+export default data;
